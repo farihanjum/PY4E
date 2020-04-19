b50d41b6c8b99f46ff15e4f17f17d2cfd71a01fb
farihanjum <farihaanjum.24csedu.022@gmail.com>
2020-04-19 13:29:01 +0600

    week 6

---
+++ week 6/def_with_return.py
@@ -0,0 +1,11 @@
+def greet(lang) :
+  if lang == 'es' :
+    return 'Hola'
+  elif lang == 'fr' :
+    return 'Bonjour'
+  else :
+    return 'Hello'
+
+print(greet('es'),'Rifa')
+print(greet('fr'),'Nabila')
+print(greet('en'),'Shifa')
---
+++ week 6/def_without_return.py
@@ -0,0 +1,12 @@
+def greet(lang) :
+  if lang == 'es' :
+    print ('Hola')
+  elif lang == 'fr' :
+    print ('Bonjour')
+  else :
+    print ('Hello')
+
+greet('es') #but eta print korbe
+greet('fr')
+#print(greet('en'),'Shifa') ---> ei line ta kaj kore na
+#because jokhn return use koriani tokhn function ksui return korlona.
